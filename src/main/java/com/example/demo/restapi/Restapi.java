50dadd4b7bdb9129b9c6b88706caee5cc2309047
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-12-08 14:29:50 +0100

    Add Index.html

--- src/main/java/com/example/demo/restapi/Restapi.java
+++ src/main/java/com/example/demo/restapi/Restapi.java
@@ -3,6 +3,8 @@ package com.example.demo.restapi;
 import com.example.demo.serwis.Serwis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,7 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
-@RestController
+@Controller
 public class Restapi {
 
     private Serwis serwis;
@@ -24,6 +26,10 @@ public class Restapi {
         this.serwis = serwis;
     }
 
+    @GetMapping("/")
+    public String getData(Model model) throws IOException {
+        return "index";
+    }
 
 
 }
